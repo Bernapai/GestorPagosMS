024d083df28c79859739d040b7d6406d969fa4f8
Bernapai <juanemeunier@gmail.com>
2025-02-19 18:28:27 -0300

    Changes 2.3 Almost Finishing TransactionMicro

--- Microservices/transactionsMicro/transtactions/models.py
+++ Microservices/transactionsMicro/transtactions/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Transaction(models.Model):
+    user_id = models.IntegerField()
+    amount = models.FloatField()
+    date = models.DateField()
+    description = models.CharField(max_length=100)
+    category = models.CharField(max_length=100)
+    type = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
